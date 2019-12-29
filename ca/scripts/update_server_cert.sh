d030e7f86cb5a4f6eaa61f930a15e3728bbd265a
Jukka Sarjanen <jp.sarjanen@gmail.com>
2019-12-29 12:07:07 +0200

    Create always new key when creating new server certificate

--- ca/scripts/update_server_cert.sh
+++ ca/scripts/update_server_cert.sh
@@ -3,7 +3,7 @@
 [ -z "$CA_BASEDIR" ] || [ -z "$EST_BASEDIR" ] && { echo "Error: CA_BASEDIR or EST_BASEDIR not set, call first; source ./set_env.sh"; exit 1; }
 #Create certificate signing request...
 #todo: Create also new private key
-openssl req -new -config $CA_BASEDIR/conf/est_server_cert.cnf -key $EST_BASEDIR/certs/serverkey.pem -out $CA_BASEDIR/csr_dir/servercert.csr -outform PEM
+openssl req -new -config $CA_BASEDIR/conf/est_server_cert.cnf  -newkey rsa:2048 -sha256 -nodes -keyout $EST_BASEDIR/certs/serverkey.pem -out $CA_BASEDIR/csr_dir/servercert.csr -outform PEM
 #And sign it
 openssl ca  -batch -config $CA_BASEDIR/conf/openssl-ca.cnf -policy signing_policy -extensions signing_req -out $EST_BASEDIR/certs/servercert.pem -infiles $CA_BASEDIR/csr_dir/servercert.csr
 #remove csr
