6f31169c37acf94f769e212a29028ef343a3bb62
Jukka Sarjanen <jp.sarjanen@gmail.com>
2019-12-29 11:54:59 +0200

    script fixed to create server directories and create cakey to correct place

--- ca/scripts/create_ca.sh
+++ ca/scripts/create_ca.sh
@@ -9,22 +9,25 @@
 
 #Create new CA cert and other required stuff
 touch  $CA_BASEDIR/index.txt
+touch $CA_BASEDIR/index.txt.attr
 echo '01' >  $CA_BASEDIR/serial.txt
 mkdir  $CA_BASEDIR/certs
 mkdir  $CA_BASEDIR/csr_dir
 
+mkdir $EST_BASEDIR/certs
+mkdir $EST_BASEDIR/csr_req
 
 dd if=/dev/urandom of=$CA_BASEDIR/.rnd bs=256 count=1  bs=256 count=1
 
 #Create cacert
-openssl req -x509 -config  $CA_BASEDIR/conf/openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out $CA_BASEDIR/cacert.pem -outform PEM
+openssl req -x509 -config  $CA_BASEDIR/conf/openssl-ca.cnf -newkey rsa:4096 -sha256 -keyout $CA_BASEDIR/cakey.pem -nodes -out $CA_BASEDIR/cacert.pem -outform PEM
 
 #and base64 encode it
 cp $CA_BASEDIR/cacert.pem $EST_BASEDIR/certs/
 openssl crl2pkcs7 -nocrl -certfile $EST_BASEDIR/certs/cacert.pem -out $EST_BASEDIR/certs/cacert.p7b
 
 #Create key and csr for our est server....
-pushd ../server/certs
+pushd $EST_BASEDIR/certs
 openssl req -config  $CA_BASEDIR/conf/est_server_cert.cnf -newkey rsa:2048 -sha256 -nodes -out $CA_BASEDIR/csr_dir/servercert.csr -outform PEM
 popd
 
